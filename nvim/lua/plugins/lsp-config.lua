9a9a3add5a4996a5a2970bac0711d44056b34597
Ata Susamaz <ata@susamaz.com>
2024-04-30 21:05:54 -0400

    changes :)

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,9 +22,8 @@ return {
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-
-			vim.diagnostic.config({ update_in_insert = true })
 		end,
 	},
 }
