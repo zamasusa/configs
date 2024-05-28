7569a802c92a2aa3a07aa67afa941b2d9771ef54
Ata Susamaz <ata@susamaz.com>
2024-05-28 14:28:58 -0400

    test

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -23,12 +23,12 @@ return {
 				end,
 				sources = {
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettierd,
 					null_ls.builtins.formatting.gofmt,
 					null_ls.builtins.diagnostics.proselint,
 					require("none-ls.code_actions.eslint_d"),
 					require("none-ls.diagnostics.eslint_d"),
 					require("none-ls.formatting.eslint_d"),
+					null_ls.builtins.formatting.prettierd,
 				},
 			})
 
